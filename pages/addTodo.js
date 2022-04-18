d1be0d0175fe29dba5f7ee16ede2bc3c1a1124fd
Gavin Nimsara Gunapala <68413021+gavingunapala@users.noreply.github.com>
2022-04-18 14:31:00 +0530

    get data from form

--- pages/addTodo.js
+++ pages/addTodo.js
@@ -1,25 +1,58 @@
 import "bootstrap/dist/css/bootstrap.css";
 import styles from "../styles.module.css";
+
+import React, { useState, useEffect } from "react";
 function addtodo() {
+  const [title, settitle] = useState("");
+  const [body, setbody] = useState("");
+
+  // setters
+  const titlesetter = (e) => {
+    settitle(e.target.value);
+  };
+  const bodySetter = (e) => {
+    setbody(e.target.value);
+  };
+  const submit = (e) => {
+    e.preventDefault();
+    let data = {
+      title: title,
+      body: body,
+    };
+    console.log(data);
+  };
+
   return (
-    <div class="container">
-      <div class={styles.centerContent}>
+    <div className="container">
+      <div className={styles.centerContent}>
         <h1>Add Data</h1>
         <br />
         <a href="/home">add data</a>
         <br /> <br />
         <form id="form">
-          <div class="mb-3">
-            <label for="title" class="form-label">
+          <div className="mb-3">
+            <label htmlFor="title" className="form-label">
               Title
             </label>
-            <input type="text" id="Title" class={styles.formControl} />
+            <input
+              type="text"
+              id="Title"
+              className={styles.formControl}
+              onChange={titlesetter}
+            />
           </div>
-          <label for="Listitems" class="form-label">
+          <label htmlFor="Listitems" className="form-label">
             List
           </label>
-          <input type="text" id="ListTODO" class={styles.formControl} />
-          <button id="add">Add</button>
+          <input
+            type="text"
+            id="ListTODO"
+            className={styles.formControl}
+            onChange={bodySetter}
+          />
+          <button id="add" onClick={submit}>
+            Add
+          </button>
         </form>
       </div>
     </div>
